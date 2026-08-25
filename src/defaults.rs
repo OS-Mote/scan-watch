a932a261e6be83e6afa9513ce5cc173f41e3a41b
OS-Mote <65051226+OS-Mote@users.noreply.github.com>
2026-08-24 23:49:41 -0700

    Const.

--- src/defaults.rs
+++ src/defaults.rs
@@ -1,12 +1,12 @@
 pub static DEFAULTS_NAMESPACE_KEY: &str = "defaults";
 
-pub static DISPLAY_BRIGHTNESS_DEFAULT_KEY: &str = "dis-bri";
-pub static DISPLAY_BRIGHTNESS_DEFAULT: u8 = 127;
-pub static DISPLAY_TIMEOUT_DEFAULT_KEY: &str = "dis-tim";
-pub static DISPLAY_TIMEOUT_DEFAULT: u8 = 5;
-pub static TIMEZONE_OFFSET_DEFAULT_KEY: &str = "tz-offset";
-pub static TIMEZONE_OFFSET_DEFAULT: i32 = 0;
-pub static TIMESTAMP_DEFAULT_KEY: &str = "timest";
-pub static TIMESTAMP_DEFAULT: i64 = 0;
-pub static TIMESTAMP_SET_DEFAULT_KEY: &str = "timest-set";
-pub static TIMESTAMP_SET_DEFAULT: u64 = 0;
+pub const DISPLAY_BRIGHTNESS_DEFAULT_KEY: &str = "dis-bri";
+pub const DISPLAY_BRIGHTNESS_DEFAULT: u8 = 127;
+pub const DISPLAY_TIMEOUT_DEFAULT_KEY: &str = "dis-tim";
+pub const DISPLAY_TIMEOUT_DEFAULT: u8 = 5;
+pub const TIMEZONE_OFFSET_DEFAULT_KEY: &str = "tz-offset";
+pub const TIMEZONE_OFFSET_DEFAULT: i32 = 0;
+pub const TIMESTAMP_DEFAULT_KEY: &str = "timest";
+pub const TIMESTAMP_DEFAULT: i64 = 0;
+pub const TIMESTAMP_SET_DEFAULT_KEY: &str = "timest-set";
+pub const TIMESTAMP_SET_DEFAULT: u64 = 0;
