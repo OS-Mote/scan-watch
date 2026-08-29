ef24ae1195502d6c35b7ff116619b41dd724335f
OS-Mote <65051226+OS-Mote@users.noreply.github.com>
2026-08-29 02:38:20 -0700

    Refactored defaults into settings.

--- src/main.rs
+++ src/main.rs
@@ -115,20 +115,10 @@ mod qspi_bus;
 mod framebuffer;
 mod axp2101;
 mod co5300;
-mod defaults;
+mod settings;
 
 use crate::{
-    defaults::{
-        set_display_brightness,
-        set_time,
-        set_timezone_offset,
-        set_display_timeout,
-        get_date_time,
-        get_timezone_offset,
-        get_timestamp,
-        get_display_timeout,
-        get_display_brightness
-    },
+    settings::Settings,
     axp2101::Axp2101,
     co5300::{
         Co5300Display,
@@ -176,6 +166,7 @@ static POWER_CELL: StaticCell<CriticalSectionMutex<RefCell<Axp2101<RefCellDevice
 static FLASH_STORAGE_CELL: StaticCell<CriticalSectionMutex<RefCell<Nvs<FlashStorage<'static>>>>> = StaticCell::new();
 static DISPLAY_CELL: StaticCell<CriticalSectionMutex<RefCell<Co5300Display<'static>>>> = StaticCell::new();
 static TOUCH_CELL: StaticCell<CriticalSectionMutex<RefCell<BlockingCST92xx<RefCellDevice<'static, I2c<'static, esp_hal::Blocking>>, Delay>>>> = StaticCell::new();
+static SETTINGS_CELL: StaticCell<CriticalSectionMutex<RefCell<Settings<CriticalSectionMutex<RefCell<Nvs<FlashStorage<'static>>>>>>>> = StaticCell::new();
 
 static DISPLAY_ON_MUTEX: Mutex<CriticalSectionRawMutex, bool> = Mutex::new(true);
 
@@ -221,7 +212,7 @@ async fn main(spawner: Spawner) -> ! {
     let _ = power.init();
     let _ = power.trim_adc_channels();
 
-    let mut flash_storage = Nvs::new(0x9000, 0x14000, FlashStorage::new(peripherals.FLASH))
+    let flash_storage = Nvs::new(0x9000, 0x14000, FlashStorage::new(peripherals.FLASH))
         .expect("Flash storage initilization failed.");
     
     let spi_config = SpiConfig::default()
@@ -246,7 +237,6 @@ async fn main(spawner: Spawner) -> ! {
     let mut display = Co5300Display::new(QspiBus::new(spi, cs), reset);
 
     display.init();
-    display.set_brightness(get_display_brightness(&mut flash_storage));
 
     // Enable Tearing Effect output on CO5300 (TE pin is GPIO13)
     // Using command 0x35 (TEARON) and param 0x00 (VBlank only)
@@ -275,12 +265,18 @@ async fn main(spawner: Spawner) -> ! {
 
     let power_cell = POWER_CELL.init(CriticalSectionMutex::new(RefCell::new(power)));
     let flash_storage_cell = FLASH_STORAGE_CELL.init(CriticalSectionMutex::new(RefCell::new(flash_storage)));
+
+    let settings = Settings::new(flash_storage_cell).init();
+
+    display.set_brightness(settings.get_display_brightness());
+
+    let settings_cell = SETTINGS_CELL.init(CriticalSectionMutex::new(RefCell::new(settings)));
     let touch_cell = TOUCH_CELL.init(CriticalSectionMutex::new(RefCell::new(touch)));
     let display_cell = DISPLAY_CELL.init(CriticalSectionMutex::new(RefCell::new(display)));
 
     main_window.on_set_date(|month, day, year| {
         let date_time = critical_section::with(|cs| {
-            get_date_time(&mut flash_storage_cell.borrow(cs).borrow_mut())
+            get_date_time(&settings_cell.borrow(cs).borrow())
         });
 
         let adjusted_datetime = date_time
@@ -293,15 +289,13 @@ async fn main(spawner: Spawner) -> ! {
             .to_utc();
 
         critical_section::with(|cs| {
-            let mut storage = flash_storage_cell.borrow(cs).borrow_mut();
-
-            set_time(adjusted_datetime.timestamp_micros(), &mut storage);
+            settings_cell.borrow(cs).borrow_mut().set_timestamp(adjusted_datetime.timestamp_micros());
         });
     });
 
     main_window.on_get_date(|| {
         let date_time = critical_section::with(|cs| {
-            get_date_time(&mut flash_storage_cell.borrow(cs).borrow_mut())
+            get_date_time(&settings_cell.borrow(cs).borrow())
         });
 
         let model: Rc<VecModel<i32>> = Rc::new(VecModel::from(vec![
@@ -315,7 +309,7 @@ async fn main(spawner: Spawner) -> ! {
 
     main_window.on_set_time(|hour, minute, second| {
         let date_time = critical_section::with(|cs| {
-            get_date_time(&mut flash_storage_cell.borrow(cs).borrow_mut())
+            get_date_time(&settings_cell.borrow(cs).borrow())
         });
 
         let adjusted_datetime = date_time
@@ -328,13 +322,13 @@ async fn main(spawner: Spawner) -> ! {
             .to_utc();
 
         critical_section::with(|cs| {
-            set_time(adjusted_datetime.timestamp_micros(), &mut flash_storage_cell.borrow(cs).borrow_mut());
+            settings_cell.borrow(cs).borrow_mut().set_timestamp(adjusted_datetime.timestamp_micros());
         });
     });
 
     main_window.on_get_time(|| {
         let date_time = critical_section::with(|cs| {
-            get_date_time(&mut flash_storage_cell.borrow(cs).borrow_mut())
+            get_date_time(&settings_cell.borrow(cs).borrow())
         });
 
         let model: Rc<VecModel<i32>> = Rc::new(VecModel::from(vec![
@@ -348,37 +342,38 @@ async fn main(spawner: Spawner) -> ! {
 
     main_window.on_set_timezone_offset(|offset| {
         critical_section::with(|cs| {
-            set_timezone_offset(offset, &mut flash_storage_cell.borrow(cs).borrow_mut());
+            settings_cell.borrow(cs).borrow_mut().set_timezone_offset(offset);
         });
     });
 
     main_window.on_get_timezone_offset(|| {
         critical_section::with(|cs| {
-            get_timezone_offset(&mut flash_storage_cell.borrow(cs).borrow_mut())
+            settings_cell.borrow(cs).borrow().get_timezone_offset()
         })
     });
 
     main_window.on_set_screen_brightness(|brightness| {
         critical_section::with(|cs| {
-            set_display_brightness(brightness as u8, &mut display_cell.borrow(cs).borrow_mut(), &mut flash_storage_cell.borrow(cs).borrow_mut());
+            settings_cell.borrow(cs).borrow_mut().set_display_brightness(brightness as u8);
+            display_cell.borrow(cs).borrow_mut().set_brightness(brightness as u8);
         });
     });
 
     main_window.on_get_screen_brightness(|| {
         critical_section::with(|cs| {
-            get_display_brightness(&mut flash_storage_cell.borrow(cs).borrow_mut()) as i32
+            settings_cell.borrow(cs).borrow().get_display_brightness() as i32
         })
     });
 
     main_window.on_set_screen_timeout(|timeout| {
         critical_section::with(|cs| {
-            set_display_timeout(timeout as u8, &mut flash_storage_cell.borrow(cs).borrow_mut());
+            settings_cell.borrow(cs).borrow_mut().set_display_timeout(timeout as u8);
         });
     });
 
     main_window.on_get_screen_timeout(|| {
         critical_section::with(|cs| {
-            get_display_timeout(&mut flash_storage_cell.borrow(cs).borrow_mut()) as i32
+            settings_cell.borrow(cs).borrow().get_display_timeout() as i32
         })
     });
 
@@ -398,8 +393,8 @@ async fn main(spawner: Spawner) -> ! {
 
     spawner.spawn(battery_status_task(power_cell).unwrap());
     spawner.spawn(touch_event_update_task(touch_cell).unwrap());
-    spawner.spawn(date_time_update_task(flash_storage_cell).unwrap());
-    spawner.spawn(display_timeout_countdown_task(display_cell, flash_storage_cell).unwrap());
+    spawner.spawn(date_time_update_task(settings_cell).unwrap());
+    spawner.spawn(display_timeout_countdown_task(display_cell, settings_cell).unwrap());
     spawner.spawn(wifi_sniffing_task().unwrap());
 
     let mut last_remote_id_detection: Option<Instant> = None;
@@ -508,12 +503,12 @@ async fn touch_event_update_task(touch_cell: &'static CriticalSectionMutex<RefCe
 }
 
 #[task]
-async fn date_time_update_task(storage_cell: &'static CriticalSectionMutex<RefCell<Nvs<FlashStorage<'static>>>>) {
+async fn date_time_update_task(settings_cell: &'static CriticalSectionMutex<RefCell<Settings<CriticalSectionMutex<RefCell<Nvs<FlashStorage<'static>>>>>>>) {
     let mut last_date_time = DateTime::UNIX_EPOCH.fixed_offset();
 
     loop {
         let date_time = critical_section::with(|cs| {
-            get_date_time(&mut storage_cell.borrow(cs).borrow_mut())
+            get_date_time(&settings_cell.borrow(cs).borrow())
         });
 
         if date_time != last_date_time {
@@ -551,17 +546,17 @@ async fn battery_status_task(power_cell: &'static CriticalSectionMutex<RefCell<A
 }
 
 #[task]
-async fn display_timeout_countdown_task(display_cell: &'static CriticalSectionMutex<RefCell<Co5300Display<'static>>>, storage_cell: &'static CriticalSectionMutex<RefCell<Nvs<FlashStorage<'static>>>>) {
+async fn display_timeout_countdown_task(display_cell: &'static CriticalSectionMutex<RefCell<Co5300Display<'static>>>, settings_cell: &'static CriticalSectionMutex<RefCell<Settings<CriticalSectionMutex<RefCell<Nvs<FlashStorage<'static>>>>>>>) {
     loop {
         let display_timeout = critical_section::with(|cs| {            
             display_cell.borrow(cs).borrow_mut().display_on();
 
-            get_display_timeout(&mut storage_cell.borrow(cs).borrow_mut()) as u64
+            settings_cell.borrow(cs).borrow().get_display_timeout()
         });
 
         *DISPLAY_ON_MUTEX.lock().await = true;
 
-        Timer::after_secs(display_timeout).await;
+        Timer::after_secs(display_timeout as u64).await;
         
         if !DISPLAY_TOUCHED.signaled() {
             critical_section::with(|cs| {
@@ -672,4 +667,14 @@ async fn wifi_sniffing_task() {
             }
         }
     }
+}
+
+fn get_date_time(settings: &Settings<CriticalSectionMutex<RefCell<Nvs<FlashStorage<'static>>>>>) -> DateTime<FixedOffset> {
+    let timestamp = settings.get_timestamp();
+    let timezone_offset = settings.get_timezone_offset();
+    let now_ticks = Instant::now().as_micros();
+
+    DateTime::from_timestamp_micros(timestamp + (now_ticks as i64))
+        .unwrap()
+        .with_timezone(&FixedOffset::east_opt(3600 * timezone_offset).unwrap())
 }
---
+++ src/settings.rs
@@ -0,0 +1,111 @@
+use static_cell::StaticCell;
+use core::cell::RefCell;
+use chrono::{
+    DateTime,
+    FixedOffset
+};
+use esp_storage::FlashStorage;
+use esp_nvs::{
+    Nvs,
+    Key
+};
+use embassy_time::Instant;
+use embassy_sync::blocking_mutex::CriticalSectionMutex;
+
+const SETTINGS_NAMESPACE_KEY: &str = "settings";
+
+const DISPLAY_BRIGHTNESS_SETTINGS_KEY: &str = "dis-bri";
+const DISPLAY_BRIGHTNESS_DEFAULT: u8 = 127;
+const DISPLAY_TIMEOUT_SETTINGS_KEY: &str = "dis-tim";
+const DISPLAY_TIMEOUT_DEFAULT: u8 = 5;
+const TIMEZONE_OFFSET_SETTINGS_KEY: &str = "tz-offset";
+const TIMEZONE_OFFSET_DEFAULT: i32 = 0;
+const TIMESTAMP_SETTINGS_KEY: &str = "timest";
+const TIMESTAMP_DEFAULT: i64 = 0;
+
+pub struct Settings<S: 'static> {
+    storage: &'static S,
+    display_brightness: u8,
+    display_timeout: u8,
+    timezone_offset: i32,
+    timestamp: i64
+}
+
+impl Settings<CriticalSectionMutex<RefCell<Nvs<FlashStorage<'static>>>>> {
+    pub const fn new(storage: &'static CriticalSectionMutex<RefCell<Nvs<FlashStorage<'static>>>>) -> Self {
+        Settings {
+            storage,
+            display_brightness: DISPLAY_BRIGHTNESS_DEFAULT,
+            display_timeout: DISPLAY_TIMEOUT_DEFAULT,
+            timezone_offset: TIMEZONE_OFFSET_DEFAULT,
+            timestamp: TIMESTAMP_DEFAULT
+        } 
+    }
+
+    pub fn init(mut self) -> Self {
+        critical_section::with(|cs| {
+            self.display_brightness = self.storage.borrow(cs).borrow_mut().get(&Key::from_str(SETTINGS_NAMESPACE_KEY), &Key::from_str(DISPLAY_BRIGHTNESS_SETTINGS_KEY))
+                .unwrap_or(DISPLAY_BRIGHTNESS_DEFAULT);
+
+            self.display_timeout = self.storage.borrow(cs).borrow_mut().get(&Key::from_str(SETTINGS_NAMESPACE_KEY), &Key::from_str(DISPLAY_TIMEOUT_SETTINGS_KEY))
+                .unwrap_or(DISPLAY_TIMEOUT_DEFAULT);
+
+            self.timezone_offset = self.storage.borrow(cs).borrow_mut().get(&Key::from_str(SETTINGS_NAMESPACE_KEY), &Key::from_str(TIMEZONE_OFFSET_SETTINGS_KEY))
+                .unwrap_or(TIMEZONE_OFFSET_DEFAULT);
+
+            self.timestamp = self.storage.borrow(cs).borrow_mut().get(&Key::from_str(SETTINGS_NAMESPACE_KEY), &Key::from_str(TIMESTAMP_SETTINGS_KEY))
+                .unwrap_or(TIMESTAMP_DEFAULT)
+        });
+
+        self
+    }
+
+    pub fn get_display_brightness(&self) -> u8 {
+        self.display_brightness
+    }
+
+    pub fn set_display_brightness(&mut self, brightness: u8) {
+        critical_section::with(|cs| {
+            if self.storage.borrow(cs).borrow_mut().set(&Key::from_str(SETTINGS_NAMESPACE_KEY), &Key::from_str(DISPLAY_BRIGHTNESS_SETTINGS_KEY), brightness).is_ok() {
+                self.display_brightness = brightness;
+            }
+        });
+    }
+
+    pub fn get_display_timeout(&self) -> u8 {
+        self.display_timeout
+    }
+
+    pub fn set_display_timeout(&mut self, timeout: u8) {
+        critical_section::with(|cs| {
+            if self.storage.borrow(cs).borrow_mut().set(&Key::from_str(SETTINGS_NAMESPACE_KEY), &Key::from_str(DISPLAY_TIMEOUT_SETTINGS_KEY), timeout).is_ok() {
+                self.display_timeout = timeout;
+            }
+        });
+    }
+
+    pub fn get_timezone_offset(&self) -> i32 {
+        self.timezone_offset
+    }
+
+    pub fn set_timezone_offset(&mut self, offset: i32) {
+        critical_section::with(|cs| {
+            if self.storage.borrow(cs).borrow_mut().set(&Key::from_str(SETTINGS_NAMESPACE_KEY), &Key::from_str(TIMEZONE_OFFSET_SETTINGS_KEY), offset).is_ok() {
+                self.timezone_offset = offset;
+            }
+        });
+    }
+
+    pub fn get_timestamp(&self) -> i64 {
+        self.timestamp
+    }
+
+    pub fn set_timestamp(&mut self, timestamp: i64) {
+        critical_section::with(|cs| {
+            if self.storage.borrow(cs).borrow_mut().set(&Key::from_str(SETTINGS_NAMESPACE_KEY), &Key::from_str(TIMESTAMP_SETTINGS_KEY), timestamp).is_ok() {
+                self.timestamp = timestamp;
+            }
+        });
+    }
+
+}
